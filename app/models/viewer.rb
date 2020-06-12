0c475811a2fa6de803d4a74325b8f216bdd5ff6b
keeks5456 <Akikogreen08@gmail.com>
2020-06-12 10:59:48 -0700

    code challenge

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,26 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+
+  def reviews 
+    # binding.pry
+    Review.all.select {|review| review.viewer == self}
+  end
+
+  def reviewed_movies
+    # binding.pry
+    reviews.collect {|review| review.movie}
+  end
+
+  def reviewed_movie?(movie)
+  # look through all reivews
+  # reviewed_movies
+  reviewed_movies.include?(movie)
+    # binding.pry
+  end
+
+  # def rate_movie(movie, rating)
+
+  # end
 end
